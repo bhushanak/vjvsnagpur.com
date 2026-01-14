0f91f3d1a9c10665e85679a9e626ad068c391ec5
bhushanak <bhushanak@rediffmail.com>
2026-01-14 19:23:00 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+    <nav>
+        <a href="#" class="logo">DON<span>AR.</span></a>
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Causes</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <a href="#" class="btn-donate">Donate Now</a>
+    </nav>
